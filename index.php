e28687fd968b03d5ff208354a3516f4c66fc4d65
Shakil <shakil.ahmed2@northsouth.edu>
2024-10-07 05:10:46 +0600

    css updated

--- index.php
+++ index.php
@@ -12,32 +12,37 @@
         <link rel="stylesheet" href="styles/style.css">
     </head>
     <body>
-        <header> 
+        <header>
             Notes App
         </header>
-        <div>
-                <div>
-                    <a class="nav-link" href="new.php">Add a new note</a>
-                </div>
-                    <?php
-                    while ($note = mysqli_fetch_assoc($notes)){
-                        ?>
-                    <div class="note">
-                        <div class="titleContainer">
-                            <span class="nt-title"><?php echo $note['title']; ?></span>
-                            <div class="nt-links">
-                                <a class="nt-link" href=<?php echo 'edit.php?id='. $note['id']; ?>>edit note</a>
-                                <a class="nt-link" href=<?php echo 'delete.php?id='. $note['id']; ?>>[X] delete note</a>
-                            </div>                 
-                        </div>
-                    
-                         <div class="nt-content"><?php if ($note['important']) {
-                            echo "<span class='imp'>IMPORTANT</span>";
-                        } ?><?php echo $note['content']; ?></div>
+        
+        <div class="titleDiv">
+            <div class="backLink">
+                <a class="nav-link" href="new.php">Add a new note</a>
+            </div>
+        </div>
+
+        <div class="note-container">
+            <?php
+            while ($note = mysqli_fetch_assoc($notes)) {
+                ?>
+                <div class="note">
+                    <div class="titleContainer">
+                        <span class="nt-title"><?php echo htmlspecialchars($note['title']); ?></span>
+                        <div class="nt-links">
+                            <a class="nt-link" href="<?php echo 'edit.php?id='. $note['id']; ?>">Edit</a>
+                            <a class="nt-link" href="<?php echo 'delete.php?id='. $note['id']; ?>">[X] Delete</a>
+                        </div>                 
                     </div>
-                    <?php }
-                    mysqli_free_result($notes);
-                    ?>
+
+                    <div class="nt-content">
+                        <?php if ($note['important']) { echo "<span class='imp'>IMPORTANT</span><br>"; } ?>
+                        <?php echo htmlspecialchars($note['content']); ?>
+                    </div>
+                </div>
+            <?php }
+            mysqli_free_result($notes);
+            ?>
         </div> 
     </body>
 </html>
